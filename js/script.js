fc9aa8c63a7ee3616a9766f64a156adb0729c60b
Ruan Lins <ruanblins@gmail.com>
2023-05-09 13:40:51 -0300

    First commit

---
+++ js/script.js
@@ -0,0 +1,23 @@
+import initScrollSuave from './modules/scroll-suave';
+import initAnimacaoScroll from './modules/scroll-animacao';
+import initAccordion from './modules/accordion';
+import initTabNav from './modules/tabnav';
+import initModal from './modules/modal';
+import initTooltip from './modules/tooltip';
+import initDropdownMenu from './modules/dropdown-menu';
+import initMenuMobile from './modules/menu-mobile';
+import initFuncionamento from './modules/funcionamento';
+import initFetchAnimais from './modules/fetch-animais';
+import initFetchBitcoin from './modules/fetch-bitcoin';
+
+initScrollSuave();
+initAnimacaoScroll();
+initAccordion();
+initTabNav();
+initModal();
+initTooltip();
+initDropdownMenu();
+initMenuMobile();
+initFuncionamento();
+initFetchAnimais();
+initFetchBitcoin();
