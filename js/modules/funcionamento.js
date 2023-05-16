aeca1a555cd179413c27d55cf3256b1304727dff
Ruan Lins <ruanblins@gmail.com>
2023-05-15 23:06:27 -0300

    Working days function remade as a class

--- js/modules/funcionamento.js
+++ js/modules/funcionamento.js
@@ -1,16 +1,38 @@
-export default function initFuncionamento() {
-  const funcionamento = document.querySelector('[data-semana]');
-  const diasSemana = funcionamento.dataset.semana.split(',').map(Number);
-  const horarioSemana = funcionamento.dataset.horario.split(',').map(Number);
-  
-  const dataAgora = new Date();
-  const diaAgora = dataAgora.getDay();
-  const horarioAgora = dataAgora.getHours();
-  
-  const semanaAberto = diasSemana.indexOf(diaAgora) !== -1;
-  const horarioAberto = (horarioAgora >= horarioSemana[0] && horarioAgora < horarioSemana[1]);
-  
-  if(semanaAberto && horarioAberto) {
-    funcionamento.classList.add('aberto');
+export default class Workingdays {
+  constructor(workingtime, activeCLass) {
+    this.workingtime = document.querySelector(workingtime);
+    this.activeCLass = activeCLass;
+  }
+
+  workingData() {
+    this.weekDays = this.workingtime.dataset.semana.split(',').map(Number);
+    this.weekHour = this.workingtime.dataset.horario.split(',').map(Number);
+  }
+
+  todayData() {
+    this.dateNow = new Date();
+    this.todayDate = this.dateNow.getDay();
+    this.todayHour = this.dateNow.getUTCHours() - 3;
+  }
+
+  isOpen() {
+    const openDay = this.weekDays.indexOf(this.todayDate) !== -1;
+    const openHour = (this.todayHour >= this.weekHour[0] && this.todayHour < this.weekHour[1]);
+
+    return openDay && openHour;
+  }
+
+  activeOpen() {
+    if (this.isOpen()) {
+      this.workingtime.classList.add(this.activeCLass);
+    }
+  }
+
+  init() {
+    if (this.workingtime) {
+      this.workingData();
+      this.todayData();
+      this.activeOpen();
+    }
   }
 }
--- js/script.js
+++ js/script.js
@@ -6,7 +6,7 @@ import Modal from './modules/modal';
 import Tooltip from './modules/tooltip';
 import DropdownMenu from './modules/dropdown-menu';
 import MenuMobile from './modules/menu-mobile';
-import initFuncionamento from './modules/funcionamento';
+import Workingdays from './modules/funcionamento';
 import fetchAnimals from './modules/fetch-animais';
 import fetchBitcoin from './modules/fetch-bitcoin';
 
@@ -34,8 +34,9 @@ dropdownMenu.init();
 const menuMobile = new MenuMobile('[data-menu="button"]', '[data-menu="list"]');
 menuMobile.init();
 
+const funcionamento = new Workingdays('[data-semana]', 'aberto');
+funcionamento.init();
+
 fetchAnimals('../../animaisapi.json', '.numeros-grid');
 
 fetchBitcoin('https://blockchain.info/ticker', '.btc-preco');
-
-initFuncionamento();
